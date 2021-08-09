6fecae4215bc606d1bcc25c4d427014a4b03edb0
vscalcione <vincenzo99.scalcione@gmail.com>
2021-08-09 20:58:19 +0200

    Initial commit

---
+++ index.js
@@ -0,0 +1,16 @@
+const express = require('express');
+
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.set('view engine', 'ejs');
+app.set('views', __dirname + '/views');
+app.use(express.static(__dirname + '/public'));
+
+
+
+app.get('/', (req, res) => {
+    res.render('index', { foo: 'FOO'});
+})
+
+app.listen(port, () => console.log('App listening on port', port));
