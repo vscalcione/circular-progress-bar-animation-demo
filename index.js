c35a4553bc34f085c3b17c66d5749c3c0385f01d
vscalcione <vincenzo99.scalcione@gmail.com>
2021-08-10 09:39:49 +0200

    Refactoring code

--- index.js
+++ index.js
@@ -7,10 +7,6 @@ app.set('view engine', 'ejs');
 app.set('views', __dirname + '/views');
 app.use(express.static(__dirname + '/public'));
 
-
-
-app.get('/', (req, res) => {
-    res.render('index', { foo: 'FOO'});
-})
+app.get('/', (req, res) => res.render('index', { foo: 'FOO'}));
 
 app.listen(port, () => console.log('App listening on port', port));
